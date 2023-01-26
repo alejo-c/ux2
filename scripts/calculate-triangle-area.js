3128785825c766db0c8673328379dacff4a66ca0
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-01-26 18:55:40 -0500

    Add Calculate Triangle Area page
    It was created the Modal Dialog Component to show output related to
    errors and results.
    
    It were added different functions and events in the page elements.

---
+++ scripts/calculate-triangle-area.js
@@ -0,0 +1,76 @@
+const triangleForm = getElementById('triangle-form')
+const formInputs = getElementsByClass('form-input')
+
+for (let input of formInputs) {
+	let label = getElementById(input.name)
+	input.oninput = () => label.textContent = input.value === '' ? '' : input.placeholder
+}
+
+const isWarning = (expression, message) => {
+	if (expression)
+		openModalDialog('!Warning! Caculate Triangle Area', 'warn', message)
+	return expression
+}
+
+const isError = (expression, message) => {
+	if (expression)
+		openModalDialog('*Error* Caculate Triangle Area', 'err', message)
+	return expression
+}
+
+const atSubmit = e => {
+	const emptyWarning = 'The {} side input field is empty'
+	const nonNumericWarning = 'The {} side input field has a non-numeric value'
+	const zeroLengthWarning = 'The {} side length is 0'
+	const negativeLengthWarning = 'The {} side length is negative'
+	const errorMessage = "The Heron's formula only works for a triangle, and the length of the {} side is bigger or equal than the sum of the other two, the result will be 0 or a negative value, indicating that the sides doesn't form a triangle"
+
+	e.preventDefault()
+
+	const formData = new FormData(triangleForm)
+	let a = formData.get('a-side'),
+		b = formData.get('b-side'),
+		c = formData.get('c-side')
+
+	if (isWarning(a === '', format(emptyWarning, 'A'))) return
+	if (isWarning(b === '', format(emptyWarning, 'B'))) return
+	if (isWarning(c === '', format(emptyWarning, 'C'))) return
+
+	if (isWarning(!isNumber(a), format(nonNumericWarning, 'A'))) return
+	if (isWarning(!isNumber(b), format(nonNumericWarning, 'B'))) return
+	if (isWarning(!isNumber(c), format(nonNumericWarning, 'C'))) return
+
+	a = parseFloat(a), b = parseFloat(b), c = parseFloat(c)
+
+	if (isWarning(a === 0.0, format(zeroLengthWarning, 'A'))) return
+	if (isWarning(b === 0.0, format(zeroLengthWarning, 'B'))) return
+	if (isWarning(c === 0.0, format(zeroLengthWarning, 'C'))) return
+
+	if (isWarning(a < 0, format(negativeLengthWarning, 'A'))) return
+	if (isWarning(b < 0, format(negativeLengthWarning, 'B'))) return
+	if (isWarning(c < 0, format(negativeLengthWarning, 'C'))) return
+
+	if (isError(a >= b + c, format(errorMessage, 'A'))) return
+	if (isError(b >= c + a, format(errorMessage, 'B'))) return
+	if (isError(c >= a + b, format(errorMessage, 'C'))) return
+
+	const Δ = calculateTriangleArea(a, b, c)
+	openModalDialog(
+		'(Info) Calculate Triangle Area',
+		'info',
+		`The Triangle area is ${Δ}`
+	)
+}
+
+triangleForm.onsubmit = atSubmit
+// addEvent(triangleForm, 'submit', atSubmit)
+triangleForm.onreset = () => {
+	const formLabels = getElementsByClass('form-label')
+	for (const label of formLabels) label.textContent = ''
+}
+
+const calculateTriangleArea = (a, b, c) => {
+	// Calculate the semi-perimeter of the triangle (s)
+	const s = (a + b + c) / 2
+	return Math.sqrt(s * (s - a) * (s - b) * (s - c))
+}
