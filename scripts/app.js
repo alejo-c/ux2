b4667dab76e37e913dcb0a81c4416a749e10a009
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-02-01 21:10:07 -0500

    Add usefull functions
    Add randomNumber, toCOPCurrency and setProperty function.

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,11 @@ const addEvent = (element, type, action) => element.addEventListener(type, actio
 const isNumber = string => !isNaN(string) && !isNaN(parseFloat(string))
 
 const format = (string, value) => string.replace('{}', value)
+
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+const toCOPCurrency = num => num.toLocaleString('es-CO',
+	{ style: 'currency', currency: 'COP' }
+).replace(',00', '')
+
+const setProperty = (name, value) => document.documentElement.style.setProperty(name, value)
