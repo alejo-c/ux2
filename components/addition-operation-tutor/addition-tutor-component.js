8ba51c5174d53c1c788a4b43bfce7387d10c8850
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-02-01 22:27:00 -0500

    Fix apples display and buttons behaviour

--- components/addition-operation-tutor/addition-tutor-component.js
+++ components/addition-operation-tutor/addition-tutor-component.js
@@ -18,24 +18,20 @@ const equals = () => {
 	const answerRight = getElementById('answer-right')
 	const answerCloudLeft = getElementById('answer-cloud-left')
 	const answerCloudRight = getElementById('answer-cloud-right')
+	const equalsBtn = getElementById('equals-btn')
 
 	const answer = questionApplesLeft + questionApplesRight
-	let answerHint = Math.random() < .5 ? answer - 1 : answer + 1
+	let answerHint = Math.random() <= .5 ? answer - 1 : answer + 1
+	const isLeftCorrect = Math.random() <= .5
 
-	answerCloudLeft.disabled = false
-	answerCloudRight.disabled = false
+	answerLeft.innerText = isLeftCorrect ? answer : answerHint
+	answerRight.innerText = isLeftCorrect ? answerHint : answer
+	answerCloudLeft.onclick = isLeftCorrect ? correctOption : wrongOption
+	answerCloudRight.onclick = isLeftCorrect ? wrongOption : correctOption
 
-	if (Math.random() < .5) {
-		answerLeft.innerText = answer
-		answerRight.innerText = answerHint
-		answerCloudLeft.onclick = correctOption
-		answerCloudRight.onclick = wrongOption
-	} else {
-		answerLeft.innerText = answerHint
-		answerRight.innerText = answer
-		answerCloudLeft.onclick = wrongOption
-		answerCloudRight.onclick = correctOption
-	}
+	equalsBtn.disabled = true
+	answerCloudLeft.classList.toggle('selectable', true)
+	answerCloudRight.classList.toggle('selectable', true)
 
 	fillAnswerApples(answerLeft.innerText, answerRight.innerText)
 }
@@ -46,47 +42,85 @@ const reload = () => {
 	const answerRight = getElementById('answer-right')
 	const answerCloudLeft = getElementById('answer-cloud-left')
 	const answerCloudRight = getElementById('answer-cloud-right')
+	const equalsBtn = getElementById('equals-btn')
 
 	questionApplesLeft = randomNumber(1, 5)
 	questionApplesRight = randomNumber(1, 5)
+	const sum = `${questionApplesLeft} + ${questionApplesRight} = ?`
 
-	additionEcuation.innerText = `${questionApplesLeft} + ${questionApplesRight} = ?`
+	additionEcuation.innerHTML = sum
 	answerLeft.innerText = '?'
 	answerRight.innerText = '?'
 
-	answerCloudLeft.disabled = true
-	answerCloudRight.disabled = true
+	equalsBtn.disabled = false
+	answerCloudLeft.classList.toggle('selectable', false)
+	answerCloudRight.classList.toggle('selectable', false)
+	answerCloudLeft.onclick = null
+	answerCloudRight.onclick = null
 
+	clearApples()
 	fillQuestionApples()
 }
 
 const fillQuestionApples = () => {
 	const questionCloudLeft = getElementById('question-cloud-left')
 	const questionCloudRight = getElementById('question-cloud-right')
-	for (let i = 0; i < questionApplesLeft; i++)
-		questionCloudLeft.innerHTML += '<span class="apple"></span>'
-	for (let i = 0; i < questionApplesRight; i++)
-		questionCloudRight.innerHTML += '<span class="apple"></span>'
+	fillApplesAtCloud(questionCloudLeft, questionApplesLeft)
+	fillApplesAtCloud(questionCloudRight, questionApplesRight)
 }
 
 const fillAnswerApples = (answerApplesLeft, answerApplesRight) => {
 	const answerCloudLeft = getElementById('answer-cloud-left')
 	const answerCloudRight = getElementById('answer-cloud-right')
-	for (let i = 0; i < answerApplesLeft; i++)
-		answerCloudLeft.innerHTML += '<span class="apple"></span>'
-	for (let i = 0; i < answerApplesRight; i++)
-		answerCloudRight.innerHTML += '<span class="apple"></span>'
+	fillApplesAtCloud(answerCloudLeft, answerApplesLeft)
+	fillApplesAtCloud(answerCloudRight, answerApplesRight)
+}
+
+const fillApplesAtCloud = (cloud, apples) => {
+	let applesRows = '<div class="apples-row">'
+	for (let i = 0; i < apples; i++) {
+		applesRows += '<span class="apple"></span>'
+		if (i === 4)
+			applesRows += '</div><div class="apples-row">'
+	}
+	applesRows += '</div>'
+	cloud.innerHTML = applesRows
+}
+
+const clearApples = () => {
+	const questionCloudLeft = getElementById('question-cloud-left')
+	const questionCloudRight = getElementById('question-cloud-right')
+	const answerCloudLeft = getElementById('answer-cloud-left')
+	const answerCloudRight = getElementById('answer-cloud-right')
+	questionCloudLeft.innerHTML = ''
+	questionCloudRight.innerHTML = ''
+	answerCloudLeft.innerHTML = ''
+	answerCloudRight.innerHTML = ''
 }
 
 const correctOption = () => {
+	const additionEcuation = getElementById('addition-ecuation')
+	const answerCloudLeft = getElementById('answer-cloud-left')
+	const answerCloudRight = getElementById('answer-cloud-right')
+
+	const answer = questionApplesLeft + questionApplesRight
+	const sum = `${questionApplesLeft} + ${questionApplesRight} = ${answer}`
 	totalAnswersCount++
 	correctAnswersCount++
-	successRate = correctAnswersCount / totalAnswersCount * 100
+	let successRate = correctAnswersCount / totalAnswersCount * 100
+	successRate = Math.round(successRate * 100) / 100
+
 	openModalDialog(
 		'Well Done',
 		'correct',
-		`The answer is correct. Success rate: ${successRate}%`
+		`The answer is correct. ${sum}<br><strong>Success rate:</strong> ${successRate}%`
 	)
+
+	additionEcuation.innerHTML = sum
+	answerCloudLeft.classList.toggle('selectable', false)
+	answerCloudRight.classList.toggle('selectable', false)
+	answerCloudLeft.onclick = null
+	answerCloudRight.onclick = null
 }
 
 const wrongOption = () => {
