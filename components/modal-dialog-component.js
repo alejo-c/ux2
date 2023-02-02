cdb8d68e90c4cd7fad414f37570e3428db0e0990
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2023-02-01 20:49:36 -0500

    Add icon routes, change structure and fix close

--- components/modal-dialog-component.js
+++ components/modal-dialog-component.js
@@ -2,25 +2,28 @@ const openModalDialog = (dialog_title, dialog_type, dialog_message) => {
 	const DIALOG_IMAGE_TYPES = {
 		'info': '/images/information.png',
 		'warn': '/images/warning.png',
-		'err': '/images/error.png'
+		'err': '/images/error.png',
+		'correct': '/images/happy.png',
+		'wrong': '/images/sad.png',
+		'bill': '/images/bill.png'
 	}
 	let dialog_image = DIALOG_IMAGE_TYPES[dialog_type]
 
 	document.body.insertAdjacentHTML('afterend', `
 		<div id="modal-dialog">
-			<div id="dialog-header">${dialog_title}</div>
-			<div id="dialog-body">
-				<div id="dialog-image">${dialog_image}</div>
-				<div id="dialog-main">${dialog_message}</div>
-			</div>
-			<div id="dialog-footer">
-				<button id="dialog-close" class="btn">Ok</button>
+			<div id="dialog-main" class="scroll invert-scroll">
+				<h2 id="dialog-header">
+					<img id="dialog-image" src="${dialogImage}" alt="${dialogTitle}">
+					${dialogTitle}
+				</h2>
+				<div id="dialog-message">${dialogMessage}</div>
+				<button id="dialog-close-btn" class="btn">Ok</button>
 			</div>
 		</div>
 	`)
-
-	const dialogCloseButton = getElementById('dialog-close')
-	dialogCloseButton.onclick = closeModalDialog
+	const dialogCloseBtn = getElementById('dialog-close-btn')
+	dialogCloseBtn.focus()
+	dialogCloseBtn.onclick = closeModalDialog
 }
 
 const closeModalDialog = () => {
